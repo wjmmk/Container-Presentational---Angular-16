69007fba9000f05b4e51bded0350e5b9fd0c4f9c
William Mosquera <wjmmk3@gmail.com>
2024-10-14 23:11:21 -0500

    feat: actualizacion de archivos de configuracion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormpresentationalComponent } from './formpresentational/formpresentational.component';
+import { FormcontainerComponent } from './formcontainer/formcontainer.component';
+import { ProductocardComponent } from './productos/components/productocard/productocard.component';
+import { ProductosContainerComponent } from './productos/containers/productos-container/productos-container.component';
+import { ListComponent } from './productos/components/list/list.component';
+import { CarritoListComponent } from './carrito/components/carrito-list/carrito-list.component';
+import { CarritoContainerComponent } from './carrito/containers/carrito-container/carrito-container.component';
+import { UsuarioPerfilComponent } from './usuarios/components/usuario-perfil/usuario-perfil.component';
+import { UsuarioContainerComponent } from './usuarios/containers/usuario-container/usuario-container.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormpresentationalComponent,
+    FormcontainerComponent,
+    ProductocardComponent,
+    ProductosContainerComponent,
+    ListComponent,
+    CarritoListComponent,
+    CarritoContainerComponent,
+    UsuarioPerfilComponent,
+    UsuarioContainerComponent
   ],
   imports: [
     BrowserModule,
