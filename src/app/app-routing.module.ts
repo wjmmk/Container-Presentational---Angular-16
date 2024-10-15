0a26a53c548958da0901128f0eeae99659506688
William Mosquera <wjmmk3@gmail.com>
2024-10-14 23:10:29 -0500

    feat: ruta para visualizar los productos implementada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormcontainerComponent } from './formcontainer/formcontainer.component';
+import { ProductocardComponent } from './productos/components/productocard/productocard.component';
+import { ProductosContainerComponent } from './productos/containers/productos-container/productos-container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FormcontainerComponent},
+  { path: 'products', component: ProductosContainerComponent},
+  { path: 'products/id', component: ProductosContainerComponent},
+  { path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
